b8376fb4df57d59af4092b8324cb29268fab9008
Bibi <bibi@bibi.nu>
2023-08-16 20:11:51 +0200

    Load extension from bundle.

--- src/ObHighchartsBundle.php
+++ src/ObHighchartsBundle.php
@@ -4,8 +4,14 @@ declare(strict_types=1);
 
 namespace Ob\HighchartsBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class ObHighchartsBundle extends Bundle
+class ObHighchartsBundle extends AbstractBundle
 {
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/services.yml');
+    }
 }
